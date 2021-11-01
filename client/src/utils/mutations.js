3386cfb7de8b4ebd8db1739ebc1a43c335440efa
Alyssa harper <alyssaharper@Alyssas-Air.home>
2021-10-31 21:26:30 -0400

    Updated save book mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,5 @@
 import { gql } from '@apollo/client';
+
 export const LOGIN_USER = gql `
 mutation login ($email: String!, $password: String!) {
     login (email: $email, password: $password) {
@@ -9,4 +10,22 @@ mutation login ($email: String!, $password: String!) {
         }
     }
 }
+`
+
+export const SAVE_BOOK = gql`
+    mutation addBook($authors: [String], $description: String, $bookId: String!, $image: String!, $link: String, $title: String) {
+        addBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
 `
